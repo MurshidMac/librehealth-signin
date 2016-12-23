069358d494fb70c6002a1f1117408e7b70cfd0ae
Namrata Nehete <n.nehete@gmail.com>
2016-12-23 15:45:02 -0500

    Created owa component. Added redirect url setting. Updated bower dependencies.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 var loginPanel;
+var baseUrl;
+
 $(document).ready(function(){
-  loginPanel = document.getElementById('loginFormDiv');
-}); 
+    setBaseUrl();
+    checkAuthentication();
+    $('#loginForm').submit(function(event){
+        if(this.checkValidity())
+        {
+            event.preventDefault();
+        }
+    });
+});
+function setBaseUrl() {
+    baseUrl = window.location.origin + "/" + window.location.pathname.split('/')[1];
+}
+
+function checkAuthentication() {
+    if (sessionStorage)
+    {
+        $.ajax({
+            url: baseUrl +"/ws/rest/v1/session",
+            headers: '{"accept": "application/json"}',
+            type: "GET",
+            cache: false,
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function (data) {
+                sessionStorage.setItem("authentication", data.authenticated);
+                setAuthenticationLabel(data);
+            },
+            error: function (xhr, ajaxOptions, error) {
+                console.log("Checking authentication request failed: "+ xhr.status + " " + error);
+            }
+        });
+    }
+}
 
 function make_base_auth(user, password) {
   var tok = user + ':' + password;
@@ -10,6 +44,7 @@ function make_base_auth(user, password) {
 }
 
 function showPanel() {
+    loginPanel = document.getElementById('loginFormDiv');
     loginPanel.style.display='block';
 }
 
@@ -25,36 +60,76 @@ window.onclick = function(event) {
 }
 
 function resetAndCloseForm() {
+    $("#errorMsg").text("");
     document.getElementById("loginForm").reset();
     hidePanel();
 }
 
-function login() {
+function login(onLoginRedirectUrl) {
+    
     var username = $("input#uname").val();
-    var password = $("input#psw").val();  
+    var password = $("input#psw").val();
     if(username !== "" && password !== "") {
-        console.log('test username '+username + ' password '+ password);
         $.ajax({
-            type: "POST",
-            url: "/ws/rest/v1/session",
+            type: "GET",
+            url: baseUrl +"/ws/rest/v1/session",
             dataType: 'json',
-            async: false,
-            data: '{}',
             crossDomain: true,
-            beforeSend: function (xhr){ 
-                xhr.setRequestHeader('Authorization', make_base_auth(username, password)); 
+            contentType: 'application/json',
+            beforeSend: function (xhr){
+                xhr.setRequestHeader('Authorization', make_base_auth(username, password));
             },
             success: function (data){
                 if(data.authenticated)
                 {
-                    console.log('Hello' + " authenticated "+data.authenticated);
-                    alert('Login successful');
+                    $("#errorMsg").text("");
+                    sessionStorage.setItem("authentication", data.authenticated);
+                    setAuthenticationLabel(data);
+                    window.location.href = onLoginRedirectUrl;
                 }
-                 
+                else
+                {
+                    $("#errorMsg").text("Wrong credentials. Try again!");
+                }
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 console.log(xhr.status + " " + thrownError);
             }
         });
+
+    }
+}
+
+function logout() {
+
+    $.ajax({
+        type: "DELETE",
+        url: baseUrl +"/ws/rest/v1/session",
+        dataType: 'json',
+        crossDomain: true,
+        contentType: 'application/json',
+        success: function (data) {
+
+            sessionStorage.setItem("authentication", false);
+            setAuthenticationLabel(data);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status + " " + thrownError);
+        }
+    });
+}
+
+function setAuthenticationLabel(data) {
+    if(data && data.authenticated)
+    {
+        $("#welcomeUser").text("Welcome "+ data.user.display + "!");
+        $("#authenticationLabel").text("Logout");
+        console.log("authentication " + sessionStorage.getItem("authentication"));
+    }
+    else
+    {
+        $("#welcomeUser").text("");
+        $("#authenticationLabel").text("Login");
+        console.log("authentication " + sessionStorage.getItem("authentication"));
     }
 }
